6f5ac909682c58da30e7b86ac25490f5f1f93e53
Emmanuel Adesile <emma2adesile@gmail.com>
2019-05-02 12:36:24 +0100

    Updates

---
+++ precache-manifest.fe14b342daa446fd0637171aa4460192.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "24c12bfbc3020a160520be5a407d3d0a",
+    "url": "/moviemania/static/media/moviemania-logo.24c12bfb.svg"
+  },
+  {
+    "revision": "8bef76c911b1500c15de857684a391f7",
+    "url": "/moviemania/static/media/avengers_endgame.8bef76c9.jpg"
+  },
+  {
+    "revision": "651a09d0e7f05997a64b908ef8f68e5d",
+    "url": "/moviemania/static/media/arrow-wallpaper.651a09d0.jpg"
+  },
+  {
+    "revision": "981277a3b63f65333fc1",
+    "url": "/moviemania/static/js/runtime~main.f3ccedd2.js"
+  },
+  {
+    "revision": "a704f40f611df91d571a",
+    "url": "/moviemania/static/js/main.647115b9.chunk.js"
+  },
+  {
+    "revision": "b238bf11421965c8ca4d",
+    "url": "/moviemania/static/js/2.29888324.chunk.js"
+  },
+  {
+    "revision": "a704f40f611df91d571a",
+    "url": "/moviemania/static/css/main.18dec2b5.chunk.css"
+  },
+  {
+    "revision": "c83ee1098f4950817de33918792ad670",
+    "url": "/moviemania/index.html"
+  }
+];
