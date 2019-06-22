c25a5c80de866538e6bd863feba3cff50d0c0a05
Emmanuel Adesile <emma2adesile@gmail.com>
2019-06-22 21:55:36 +0100

    update home route to show movies

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -4,15 +4,9 @@ import Movies from "../components/Movies";
 import Tvshows from "../components/TvShows";
 import VideoDetails from "../components/VideoDetails";
 
-const Home = () => (
-  <div>
-    <h1>Welcome to Movie Mania</h1>
-  </div>
-);
-
-const Routes = () => ( 
+const Routes = () => (
   <Router>
-    <Home path="/" />
+    <Movies path="/" />
     <Movies path="/movies" />
     <Tvshows path="/tv" />
     <VideoDetails path="/tv/:videoId/" />
