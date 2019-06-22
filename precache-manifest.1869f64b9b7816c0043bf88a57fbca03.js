0c954eaaab2f249ba97f0ab4cfe7047fa71c21a9
Emmanuel Adesile <emma2adesile@gmail.com>
2019-06-22 21:43:31 +0100

    Updates

---
+++ precache-manifest.1869f64b9b7816c0043bf88a57fbca03.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cc96773e800f480a25f00ca1c1548be",
+    "url": "/moviemania/index.html"
+  },
+  {
+    "revision": "30103328091340fca40b",
+    "url": "/moviemania/static/css/main.1218d226.chunk.css"
+  },
+  {
+    "revision": "951dc43afc322eeb32f7",
+    "url": "/moviemania/static/js/2.2efb28b1.chunk.js"
+  },
+  {
+    "revision": "30103328091340fca40b",
+    "url": "/moviemania/static/js/main.d65f5eab.chunk.js"
+  },
+  {
+    "revision": "981277a3b63f65333fc1",
+    "url": "/moviemania/static/js/runtime~main.f3ccedd2.js"
+  },
+  {
+    "revision": "651a09d0e7f05997a64b908ef8f68e5d",
+    "url": "/moviemania/static/media/arrow-wallpaper.651a09d0.jpg"
+  },
+  {
+    "revision": "8bef76c911b1500c15de857684a391f7",
+    "url": "/moviemania/static/media/avengers_endgame.8bef76c9.jpg"
+  },
+  {
+    "revision": "24c12bfbc3020a160520be5a407d3d0a",
+    "url": "/moviemania/static/media/moviemania-logo.24c12bfb.svg"
+  }
+]);
